9ad886eff2ecdfb11536ea9c35c653cc0e3a7195
ArtsemV <adbwork@mail.ru>
2022-04-06 21:25:23 +0300

    delite 'cat'(search_fields)

--- rentcar/main/admin.py
+++ rentcar/main/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-# from .models import CarsTable
-
 
 class CarsTableAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'cat', 'is_published', 'photo', 'time_create', 'time_update')
     list_display_links = ('id', 'title', 'cat')
-    search_fields = ('id', 'title', 'cat')
+    search_fields = ('id', 'title',)
 
 
 class CategoryAdmin(admin.ModelAdmin):
