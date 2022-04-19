5521777008f748ef8e775039fc8b479d0253bb7d
ArtsemV <adbwork@mail.ru>
2022-04-19 20:01:00 +0300

    client's number ?

--- rentcar/main/forms.py
+++ rentcar/main/forms.py
@@ -24,6 +24,7 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    # number_client = forms.TextField(label='Номер телефона', widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = User
