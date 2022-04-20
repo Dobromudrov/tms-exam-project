9bb8ae5b16e48a72bf094c585a886cdf299a7d43
ArtsemV <adbwork@mail.ru>
2022-04-20 18:51:57 +0300

    add test (RefreshPage)

--- rentcar/main/tests.py
+++ rentcar/main/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from .models import CarsTable
+
+
+class PageRefreshTest(SimpleTestCase):
+
+    def test_index_page_status_code(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    # def test_cars_page_status_code(self):
+    #     response = self.client.get('/cars/')
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_conditions_page_status_code(self):
+        response = self.client.get('/conditions/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contacts_page_status_code(self):
+        response = self.client.get('/contacts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page_status_code(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_status_code(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
