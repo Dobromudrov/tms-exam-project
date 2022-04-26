1b3a30f54c8d7e819b906a50db9809524c6de2ec
ArtsemV <adbwork@mail.ru>
2022-04-26 14:51:07 +0300

    test login

--- rentcar/main/tests.py
+++ rentcar/main/tests.py
@@ -1,7 +1,24 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import SimpleTestCase
 
+from django.contrib.auth import authenticate, get_user_model
 from .models import CarsTable
+
+
+class LogInTest(TestCase):
+
+    def setUp(self):
+        self.credentials = {
+            'username': 'test_user',
+            'password': 'qwertyzxc'}
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
 
 
 class PageRefreshTest(SimpleTestCase):
@@ -9,10 +26,6 @@ class PageRefreshTest(SimpleTestCase):
     def test_index_page_status_code(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
-
-    # def test_cars_page_status_code(self):
-    #     response = self.client.get('/cars/')
-    #     self.assertEqual(response.status_code, 200)
 
     def test_conditions_page_status_code(self):
         response = self.client.get('/conditions/')
@@ -29,3 +42,4 @@ class PageRefreshTest(SimpleTestCase):
     def test_login_page_status_code(self):
         response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
+
